c1d2650bf982c945b22037f45d33961a9231069f
Leonel <leonel.bruzzese@gmail.com>
2024-08-01 10:40:39 -0300

    se creo el componente header

--- leonel-ilustraciones/src/app/app.module.ts
+++ leonel-ilustraciones/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
