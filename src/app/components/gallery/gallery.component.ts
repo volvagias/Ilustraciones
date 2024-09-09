1b18a4bd19dd10562fe788595636b9bb2d7a6329
Leonel <leonel.bruzzese@gmail.com>
2024-09-09 19:33:54 -0300

    Avanzando en el carrusel de imagenes.

--- src/app/components/gallery/gallery.component.ts
+++ src/app/components/gallery/gallery.component.ts
@@ -12,7 +12,10 @@ export class GalleryComponent {
 
   items: GalleryItem[] = [
     new ImageItem({ src: '/assets/images/cronicas.png', thumb: '/assets/images/cronicas.png' }),
-    new ImageItem({ src: '/assets/images/maldita-libertad-sin-logo.png', thumb: '/assets/images/maldita-libertad-sin-logo.png' })
+    new ImageItem({ src: '/assets/images/maldita-libertad-sin-logo.png', thumb: '/assets/images/maldita-libertad-sin-logo.png' }),
+    new ImageItem({ src: '/assets/images/lullabies-sin-logo.png', thumb: '/assets/images/lullabies-sin-logo.png' }),
+    new ImageItem({ src: '/assets/images/final-fire.png', thumb: '/assets/images/final-fire.png' }),
+    new ImageItem({ src: '/assets/images/magitek.png', thumb: '/assets/images/magitek.png' }),
   ];
 
   ngOnInit(): void {
