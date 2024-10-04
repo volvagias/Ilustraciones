da67e1e31242ae78448d8cb4fdb4f75168b7deea
Leonel <leonel.bruzzese@gmail.com>
2024-10-04 11:26:22 -0300

    Agregando flechita de scroll en el carousel.

--- src/app/components/carousel/carousel.component.ts
+++ src/app/components/carousel/carousel.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-carousel',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrl: './carousel.component.css'
 })
 export class CarouselComponent {
+  
+  constructor(private router: Router) {}
+
+  // función para scrollear hacia abajo desde el carousel.
+  scrollDown() {
+    // Primero nos aseguramos de que la página ha cargado completamente el componente de destino.
+    setTimeout(() => {
+      const target = document.getElementById('target-section'); 
+      // este target-section también lo coloco como un id en la etiqueta del componente "logo".
+
+      if (target) {
+        target.scrollIntoView({ behavior: 'smooth' });
+      }
+    }, 0);
+  }
 
 }
